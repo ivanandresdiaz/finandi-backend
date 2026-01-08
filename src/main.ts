927124abeb72cc011f8a4d0690640a57b6046ba4
ivanandresdiaz <ivanandresdilox@gmail.com>
2026-01-07 19:57:00 -0500

    Main

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'http://localhost:3001'],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true, // si planeas enviar cookies o tokens
+  // });
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // si planeas enviar cookies o tokens
+    origin: ['https://finandiaz.vercel.app', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   //( PreFIX )
