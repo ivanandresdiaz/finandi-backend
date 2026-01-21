3335177d9369e12c15015df05c3764297b3ac9db
ivanandresdiaz <ivanandresdilox@gmail.com>
2026-01-21 13:41:37 -0500

    Cualqueir origin a

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
   //   credentials: true, // si planeas enviar cookies o tokens
   // });
   app.enableCors({
-    origin: ['*'],
+    origin: '*',
+
     // origin: ['https://finandiaz.vercel.app', 'http://localhost:3001'],
     // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
